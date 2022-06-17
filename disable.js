ade12a3be91bc96dee7f976dd43c2b32318b76a3
liadk07 <91607695+liad07@users.noreply.github.com>
2022-06-17 11:32:31 +0300

    Update disable.js

--- disable.js
+++ disable.js
@@ -1,6 +1,16 @@
 var arr=[]
-for (var i = 0; i <document.getElementsByTagName("script").length ; i++) {
-    arr[i]=document.getElementsByTagName("style")[i]
+var arr2=[]
+for (let j = 0; j <document.getElementsByTagName("style").length ; j++) {
+    arr[j]=document.getElementsByTagName("style")[j]
+
+}
+for (let i = 0; i <document.getElementsByTagName("link").length ; i++) {
+    if(document.getElementsByTagName("link")[i].rel=="stylesheet"){
+        arr2[i]=document.getElementsByTagName("link");
+    }
+}
+for (let bx = 1; bx <arr2.length ; bx++) {
+    document.getElementsByTagName("link")[bx].remove();
 }
 for (let i = 0; i < arr.length; i++) {
 arr[i].remove()
